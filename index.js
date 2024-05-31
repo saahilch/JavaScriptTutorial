1d992d1c8b3bb442f2a4a3b793a3ffd3c877aaab
saahilch <147494979+saahilch@users.noreply.github.com>
2024-06-01 04:47:30 +0530

    6th  commit

--- index.js
+++ index.js
@@ -1,1 +1,2 @@
+// it is use to print on consol.
 console.log("Hellow Sahil")
---
+++ variables&dataypes.js
@@ -0,0 +1,11 @@
+{
+    let a=65;
+    console.log(a)
+}
+
+a="sahil"
+console.log(a)
+const b=64;
+console.log(b)
+b="sahil"
+console.log(b)
