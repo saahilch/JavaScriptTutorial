080007d2874d69700bb943081cb07782819b5fec
saahilch <147494979+saahilch@users.noreply.github.com>
2024-06-01 05:53:14 +0530

    commited

--- variables&dataypes.js
+++ variables&dataypes.js
@@ -1,11 +1,16 @@
-{
-    let a=65;
-    console.log(a)
+let a=null;
+let b=345;
+let c=true;
+let d=BigInt("567")+BigInt("3");
+let e="Sahil";
+let f=Symbol("Im Coder")
+let g
+console.log(a,b,c,d,e,f,g);
+console.log(typeof c)
+// here key is item & rest of the stuff is objcts
+const item={
+    "Sahil":true,
+    "Sunny":false,
+    "You":undefined
 }
-
-a="sahil"
-console.log(a)
-const b=64;
-console.log(b)
-b="sahil"
-console.log(b)
+console.log(item["Sahil"])
