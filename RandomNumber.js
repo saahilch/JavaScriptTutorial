6eaf99c98023ee7226f1812c973fc901d6f1ed06
saahilch <147494979+saahilch@users.noreply.github.com>
2024-06-01 16:26:53 +0530

    commited

---
+++ RandomNumber.js
@@ -0,0 +1,15 @@
+const randomNumber = Math.floor(Math.random() * 100) + 1;
+let userGuess = prompt("Guess the number between 1 and 100");
+let turns = 0;
+
+while (userGuess != randomNumber) {
+  if (userGuess < randomNumber) {
+    userGuess = prompt("Your guess was too low. Try again.");
+  } else {
+    userGuess = prompt("Your guess was too high. Try again.");
+  }
+  turns++;
+}
+
+const score = 100 - turns;
+console.log(`Congratulations! You guessed the number in ${turns} turns! Your score is ${score}.`);
