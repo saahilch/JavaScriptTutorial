5f9e677399874c3d24d0e43fa6b5b490ae0f1f4e
saahilch <147494979+saahilch@users.noreply.github.com>
2024-06-03 02:00:22 +0530

    commmited

---
+++ map/Excersie/EX1.js
@@ -0,0 +1,6 @@
+const changeBodyRed=()=>{
+    document.body.firstElementChild.computedStyleMap.background=red
+}
+let b=document.body
+console.log("First child of b is ",b.firstChild)
+console.log("First Element child of b is",b.firstElementChild)
