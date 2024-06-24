47d560fcd2b5a1d52b512d065e24f86bb9a90e12
saahilch <147494979+saahilch@users.noreply.github.com>
2024-06-24 20:05:53 +0530

    commited

---
+++ Assignemt1.js
@@ -0,0 +1,12 @@
+let a=10;
+let b=10;
+console.log(a+b)
+console.log(typeof (a+b))
+//lets see value is changed or not.its not onlh objct value can be changed.
+const c={
+    name:"Sahil",
+    section:1,
+    isName:true
+}
+console.log(c)
+
