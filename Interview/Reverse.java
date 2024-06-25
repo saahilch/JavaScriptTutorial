df838edb69806f8701519050a8056921d376c80b
saahilch <147494979+saahilch@users.noreply.github.com>
2024-06-26 01:11:00 +0530

    commited

---
+++ Interview/Reverse.java
@@ -0,0 +1,17 @@
+package Interview;
+
+import java.util.Scanner;
+
+class Reverse {
+    public static void main(String[] args)
+
+    {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        char[] arr = str.toCharArray();
+        for (int i = 0; i < arr.length - 1; i++) {
+            System.out.println(i);
+
+        }
+    }
+}
