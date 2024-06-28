70a2beabfbd8d14dcc9281f049ac02aba91d72ef
saahilch <147494979+saahilch@users.noreply.github.com>
2024-06-28 12:13:12 +0530

    commited

---
+++ Interview/Hoisting.js
@@ -0,0 +1,54 @@
+
+       // // // console.log(y);
+       // // // var y=10;
+       // // // console.log(y); 
+
+       // // let p={
+       // //        name:"sahil",
+       // //        age:24,
+       // //        greet:function(){
+       // //               console.log("my name is"+this.name)
+       // //        }
+
+       // // };
+       // // console.log(p.name);
+       // // console.log(p.age);
+       // // // functoin called here
+       // // p.greet()
+
+       // console.log(10===10);
+       // console.log(10==10);
+
+ 
+       // function increment(x){
+       //        x=x+1;
+       //        console.log("we are in inside the function",x);
+       // }
+       // let num=10;
+       // increment(num);
+       // console.log("now we are outside the function",num);
+
+       // function add(item,list){
+       //        list.push(item);
+
+       // }
+       // let shopping=['aaa','bbb'];
+       // add('oo',shopping)
+       // console.log(shopping)
+
+       // function reverseString(str){
+       //        return str.split();
+       // }
+       // let originalString="Hellow world";
+       // let reversedString=reverseString(originalString);
+       // console.log(reversedString);
+       let array=["a", "b", "c", "a", "d","b"];
+
+       function removeDuplicates(array){
+              return array.filter((i,index)=>array.indexOf(i))
+       }
+       console.log(removeDuplicates(array))
+
+       // freecode camp YT channel  https://youtu.be/PkZNo7MFNFg?si=fFYDTPYPUmqtR4_I
+
+       // https://youtube.com/playlist?list=PLUvKRKFIL7gt3hvh5SZJ9TrlcIRaM_AfJ&si=rmI9fPuJ55_W_n_x
